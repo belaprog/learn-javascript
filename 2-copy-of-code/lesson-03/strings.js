c6f58ac4cf8372624795718f4d694dfc1b7ac084
belaprog <belprogramming@gmail.com>
2023-05-23 00:33:11 +0700

    Add lesson 3 code

---
+++ 2-copy-of-code/lesson-03/strings.js
@@ -0,0 +1,26 @@
+console.log('hello');
+alert('hello');
+
+console.log('some' + 'text');
+console.log('some' + 'more' + 'text');
+
+console.log(typeof 2);
+console.log(typeof 'hello');
+
+console.log('hello' + 3);
+console.log('$' + 20.95 + 7.99);
+console.log('$' + (20.95 + 7.99));
+console.log('$' + (2095 + 799) / 100);
+
+console.log('Item (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+console.log('hello');
+console.log("I'm learning JavaScript");
+
+console.log("I'm learning JavaScript");
+alert('some\ntext');
+
+console.log(`hello`);
+console.log(`Item (${2 + 2}): $${(2095 + 799) / 100}`);
+console.log(`some
+text`);
