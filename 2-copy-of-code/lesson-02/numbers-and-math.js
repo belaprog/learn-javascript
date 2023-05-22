48c86127a653f8b8432db57c261cf978ddef9962
belaprog <belprogramming@gmail.com>
2023-05-22 23:35:11 +0700

    Add code for lesson 1, 2 and partial code for lesson 3

---
+++ 2-copy-of-code/lesson-02/numbers-and-math.js
@@ -0,0 +1,32 @@
+// Basics of Math
+console.log(2 + 2);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 2);
+
+console.log(2 + 2 + 2);
+console.log(2.2 + 2.2);
+
+console.log(10.9 * 2 + 20.95);
+console.log(10.9 * 2 + 20.95 + 4.99);
+
+// Order of operations:
+console.log(1 + 1 * 3);
+console.log((1 + 1) * 3);
+
+console.log(20.95 + 7.99 * 2);
+console.log(36.93 * 0.1);
+console.log((20.95 + 7.99 * 2) * 0.1);
+
+// Inaccuracies with Floats:
+console.log(0.1 + 0.2);
+console.log(20.95 + 7.99);
+// use sen to counting money for accuracy
+console.log((2095 + 799) / 100);
+
+// How to round numbers:
+console.log(Math.round(2.2));
+console.log(Math.round(2.8));
+
+console.log(((2095 + 799) * 0.1) / 100);
+console.log(Math.round(((2095 + 799) * 0.1) / 100));
